ecd5a6181a0d07cb106a69f72f97fab28fcd9a3c
Borisb127 <boris.borochov92@gmail.com>
2026-01-04 21:32:25 +0200

    Render meme image and top text on canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    renderMeme()
+
+}
+
+
+function renderMeme() {
+
+    console.log('renderMeme called')
+
+    // gCtx.fillStyle = 'lightgray'
+    // gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+
+    const elImg = new Image()
+    elImg.src = 'meme-imgs/meme-imgs (square)/1.jpg'
+
+
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        gCtx.font = '40px Arial'
+        gCtx.textAlign = 'center'
+        gCtx.fillText('TEST', gElCanvas.width / 2, 50)
+
+    }
+}

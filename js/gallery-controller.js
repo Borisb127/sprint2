658552285d31a2eb53dcd224128caa3e7f13f286
Borisb127 <boris.borochov92@gmail.com>
2026-01-06 20:01:04 +0200

    Add responsive grid layout

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -10,6 +10,9 @@ function renderGallery() {
     const strHtml = `
         <img src="meme-imgs/meme-imgs (square)/1.jpg" width="150" onclick="onImgSelect(1)">
         <img src="meme-imgs/meme-imgs (square)/2.jpg" width="150" onclick="onImgSelect(2)">
+        <img src="meme-imgs/meme-imgs (square)/3.jpg" width="150" onclick="onImgSelect(3)">
+        <img src="meme-imgs/meme-imgs (square)/4.jpg" width="150" onclick="onImgSelect(4)">
+
     `
     document.querySelector('.gallery-section').innerHTML += strHtml
 
@@ -25,8 +28,8 @@ function renderGallery() {
 function onImgSelect(imgId) {
     setImg(imgId)
 
-    document.querySelector('.gallery-section').classList.add('hidden')
-    document.querySelector('.editor-section').classList.remove('hidden')
+    // document.querySelector('.gallery-section').classList.add('hidden')
+    // document.querySelector('.editor-section').classList.remove('hidden')
 
     renderMeme()
 }
